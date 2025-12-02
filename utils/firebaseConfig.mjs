8afaa9de48047f9d4c71eed871ab30f1e1e59f61
Rohan Nigam <52700885+rohan-pixel@users.noreply.github.com>
2025-12-02 17:06:50 +0530

    Update Firebase configuration credentials
    Updated Firebase configuration with new credentials.

--- utils/firebaseConfig.mjs
+++ utils/firebaseConfig.mjs
@@ -4,13 +4,13 @@ import { getDatabase } from "@firebase/database";
 import 'firebase/compat/storage';
 
 const firebaseConfig = {
-      apiKey: 'AIzaSyBzCSm-8zPX8mBg0JIWbiPsVihWApFwHdo',
-      authDomain: 'hindu-fm-2024.firebaseapp.com',
-      databaseURL: 'https://hindu-fm-2024-default-rtdb.asia-southeast1.firebasedatabase.app',
-      projectId: 'hindu-fm-2024',
-      storageBucket: 'hindu-fm-2024.appspot.com',
-      messagingSenderId:'66685597035',
-      appId: '1:66685597035:android:22bc7e749d2ee83703882e',
+      apiKey: 'AIzaSyBzCSm-xxxxxxxxxxxxxxxxxx',
+      authDomain: 'xxxxxxxxxxxxxx.firebaseapp.com',
+      databaseURL: 'https://xxxxxxxxxxxxxxxxxxxx.asia-southeast1.firebasedatabase.app',
+      projectId: 'xxxxxxxxxxxxxx',
+      storageBucket: 'xxxxxxxxxxxxxxxxx.appspot.com',
+      messagingSenderId:'xxxxxx7035',
+      appId: '1:xxxxxxxxxxxx:android:xxxxxxxxxxxxxx882e',
 }
 
 if(firebase.apps.length === 0){
